0ac93b34fa4792ac1f5018720fef28e5842a1c32
taylorzukowski <79670105+taylorzukowski@users.noreply.github.com>
2021-04-05 23:48:34 -0700

    Update

--- sketch.js
+++ sketch.js
@@ -1,59 +1,62 @@
 //cake flavors
-let cake = [{
-  name: "vanilla",
-  frosting: "chocolate"
-}, {
-  name: "confetti",
-  frosting: "nutella"
-}, {
-  name: "carrot",
-  frosting: "cream cheese"
-}, {
-  name: "lemon",
-  frosting: "vanilla"
-}, {
-  name: "chocolate",
-  frosting: "peanut butter"
-}, {
-  name: "red velvet",
-  frosting: "oreo"
-}];
+let cake = [];
 
 let randomIndex;
 let animating = false;
 
-let cakepic = [];
-let imageCounter = 0;
+let Cakepic = [];
+let imageCounter = 1;
+let startRandomizerbutton;
+let addMoreButton;
+let cnv;
+let nameInputs = [];
+let firstTime = true;
 
 function preload() {
 
   for (let i = 1; i <= 13; i++) {
-    cakepic[i] = loadImage("assets/Cakepic_" + i + ".jpeg")
+  Cakepic[i] = loadImage("assets/Cakepic_" + i + ".jpeg")
+
   }
 }
 
 function setup() {
-  console.log("HEY");
-  createCanvas(600, 600);
-  background(200);
+  cnv = createCanvas(600, 600);
+  cnv.parent("#canvasDiv");
+  background(300);
   textSize(32);
   imageMode(CENTER);
   frameRate(8);
 
-  text("click to randomize", 175, 50);
-  console.log(cakepic);
+  text("Type in Cake Flavors", 175, 50);
+  text("& Click to Randomize!", 175, 100);
 
+//  button = createButton("click to randomize");
+  startRandomizerbutton = select("#randButton")
+  startRandomizerbutton.mousePressed(buttonPressed);
+  startRandomizerbutton.style("padding", "20px");
+  startRandomizerbutton.style("background-color", "#fab9ef");
+
+  addMoreButton = select("#addMoreButton")
+  addMoreButton.mousePressed(addAnotherInput);
+  addMoreButton.style("padding", "20px");
+  addMoreButton.style("background-color", "#fab9ef");
+
+for (let i = 0; i < 3; i++){
+nameInputs.push(createInput());
+nameInputs[nameInputs.length - 1].parent("#inputFields");
+
+}
 }
 
 function draw() {
 
   if (animating == true) {
     clear();
-    image(cakepic[imageCounter], width / 2, height / 2);
+    image(Cakepic[imageCounter], width/2, height/2);
 
-    if (imageCounter < cakepic.length - 1) {
+    if (imageCounter < Cakepic.length - 1) {
       imageCounter++;
-      console.log(imageCounter);
     } else {
       imageCounter = 0;
     }
@@ -69,17 +72,32 @@ function randomizer() {
     randomIndex = int(random(cake.length));
     //text('${cake[randomIndex].name}'s frosting is
     // ${cake[randomIndex].frosting}', 50, 50);
-    text(cake[randomIndex].name + "'s frosting is " + cake[randomIndex].frosting, 75, height - 15);
-    image(random(cakepic), width / 2, height / 2);
+    //text(cake[randomIndex] + "'s frosting is " + cake[randomIndex].frosting, 75, height - 15);
+    image(random(Cakepic), width / 2, height / 2);
+    text(cake[randomIndex], width / 2, height - 15);
     cake.splice(randomIndex, 1);
   } else {
     background(random(200, 255));
-    text("nothing left!", 100, 50);
+    text("nothing left!", width / 2, height - 55);
   }
 
 }
 
-function mousePressed() {
+function addAnotherInput(){
+  nameInputs.push(createInput());
+  nameInputs[nameInputs.length - 1].parent("#inputFields");
+
+}
+
+function buttonPressed() {
+
+  if (firstTime == true){
+for (let i = 1; i < nameInputs.length; i++){
+  cake.push(nameInputs[i].value());
+}
+firstTime = false
+}
+
   animating = true;
   setTimeout(randomizer, 1000);
 
